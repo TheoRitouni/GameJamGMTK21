2f498f44f26d79621b0979568170cecd78e87318
RemyGinoyer <r.ginoyer@student.isartdigital.com>
2021-06-12 02:53:27 +0200

    Add Tag Enemy & Ground + Create Data Screenshake

---
+++ Assets/Scripts/BallVisual.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallVisual : MonoBehaviour
+{
+    private Ball ball;
+
+    [Header("ScreenShakes")]
+    [SerializeField] ShakeData[] shakeDataList;
+
+    private void Start()
+    {
+        ball = GetComponent<Ball>();
+        ball.onHitGround += OnHitGround;
+        ball.onHitEnemy += OnHitEnemy;
+    }
+
+    void OnHitGround()
+    {
+        ShakeManager.getInstance().Shake(shakeDataList[0]);
+    }
+
+    void OnHitEnemy()
+    {
+        ShakeManager.getInstance().Shake(shakeDataList[1]);
+    }
+}
---
+++ Assets/Scripts/Utils/TagList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace utils
+{
+    public class TagList
+    {
+        private static string _groundTag = "Ground";
+        private static string _enemyTag = "Enemy";
+
+        public static string groundTag => _groundTag;
+        public static string enemyTag => _enemyTag;
+    }
+}
+
