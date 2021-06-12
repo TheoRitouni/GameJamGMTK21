e862c192e8a28d67effd1bec604c1e07f582431e
leo grard <l.grard@student.isartdigital.com>
2021-06-12 20:27:26 +0200

    Add ragdoll to player character

---
+++ Assets/Scripts/Editor/BasePreview.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BasePreview : EditorWindow
+{
+    private static Vector2 size = new Vector2(37.5f,19.5f);
+    private static bool drawBorder = true;
+
+    [MenuItem("jamtools/Preview base")]
+    static void newWindow()
+    {
+        CreateWindow<BasePreview>();
+    }
+
+    private void OnEnable()
+    {
+        SceneView.beforeSceneGui += OnSceneGUI;
+    }
+    
+    private void OnDisable()
+    {
+        SceneView.beforeSceneGui -= OnSceneGUI;
+    }
+
+    private static void OnSceneGUI(SceneView pScene)
+    {
+        if(drawBorder)
+            Handles.DrawWireCube(Vector3.zero, new Vector3(size.x, size.y, 0));
+    }
+
+    private void OnGUI()
+    {
+        size = EditorGUILayout.Vector2Field("Border size", size);
+        drawBorder = EditorGUILayout.Toggle("Draw border", drawBorder);
+    }
+}
